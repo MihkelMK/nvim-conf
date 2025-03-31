e226b3e5f383fa454e1cd89961f0b7e00d417305
Mihkel Martin Kasterpalu <qpeuitovxg@use.startmail.com>
2025-03-31 20:40:34 +0300

    Redo custom mappings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -34,61 +34,15 @@ return {
       pattern = {
         [".*/etc/foo/.*"] = "fooscript",
       },
-      -- vim options can be configured here
-      options = {
-        opt = { -- vim.opt.<key>
-          relativenumber = true, -- sets vim.opt.relativenumber
-          number = true, -- sets vim.opt.number
-          spell = true, -- sets vim.opt.spell
-          signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-          wrap = true, -- sets vim.opt.wrap
-        },
-      },
-      -- Mappings can be configured through AstroCore as well.
-      -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-      mappings = {
-        -- first key is the mode
-        n = {
-          -- Disable arrows for movement
-          ["<Down>"] = false,
-          ["<Left>"] = false,
-          ["<Right>"] = false,
-          ["<Up>"] = false,
-
-          -- Disable defaults
-          ["<C-q>"] = false,
-          ["q:"] = ":",
-
-          -- navigate buffer tabs with `H` and `L`
-          L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-          H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-          -- mappings seen under group name "Buffer"
-          ["<Leader>bD"] = {
-            function()
-              require("astroui.status.heirline").buffer_picker(
-                function(bufnr) require("astrocore.buffer").close(bufnr) end
-              )
-            end,
-            desc = "Pick to close",
-          },
-          -- tables with just a `desc` key will be registered with which-key if it's installed
-          -- this is useful for naming menus
-          ["<Leader>b"] = { desc = "Buffers" },
-          -- quick save
-          ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
-        },
-        t = {
-          ["<esc><esc>"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" },
-
-          -- Remove defaults
-          ["<C-Down>"] = false,
-          ["<C-Left>"] = false,
-          ["<C-Right>"] = false,
-          ["<C-Up>"] = false,
-          ["<C-q>"] = false,
-          ["q:"] = ":",
-        },
+    },
+    -- vim options can be configured here
+    options = {
+      opt = { -- vim.opt.<key>
+        relativenumber = true, -- sets vim.opt.relativenumber
+        number = true, -- sets vim.opt.number
+        spell = true, -- sets vim.opt.spell
+        signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+        wrap = true, -- sets vim.opt.wrap
       },
     },
   },
